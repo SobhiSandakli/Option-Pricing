b769ece7fd6c1a7b69a9d913b2738d271a9addff
SobhiSandakli <sobhi.sandakli@gmail.com>
2025-01-17 10:06:49 -0500

    Set up deployment: CI/CD for C++ compilation, Vercel frontend, and server config

--- server/app.py
+++ server/app.py
@@ -6,12 +6,22 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-BLACK_SCHOLES_BINARY = "./calculations/black_scholes"
-MONTE_CARLO_BINARY = "./calculations/monte_carlo"
-BINOMIAL_TREE_BINARY = "./calculations/binomial_tree"
+import os
+
+app_root = os.path.dirname(os.path.abspath(__file__))  # Gets the absolute path of the current script
+BLACK_SCHOLES_BINARY = os.path.join(app_root, 'calculations', 'black_scholes')
+MONTE_CARLO_BINARY = os.path.join(app_root, 'calculations', 'monte_carlo')
+BINOMIAL_TREE_BINARY = os.path.join(app_root, 'calculations', 'binomial_tree')
+
+
+
+@app.route('/')
+def home():
+    return 'The api.optimap.ca is running! Thanks for checking it out! \n Check out optimap.ca for more information.'
 
 @app.route('/heatmap-data', methods=['POST'])
 def heatmap_data():
+
     try:
         data = request.json
         spot_prices = data.get('spotPrices', [])
